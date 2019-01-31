3d2bfe4caaa02e81f9a5a3bdf899c66262f87a09
Daniel Barclay <barclaysd@me.com>
2019-01-31 17:33:28 +0000

    serviceWorker caches key application data and matches request on fetch to view cached content for offline use

--- public/serviceWorker.js
+++ public/serviceWorker.js
@@ -1,7 +1,37 @@
-self.addEventListener('install', () => {
+self.addEventListener('install', (event) => {
    console.log('service worker installed');
+   event.waitUntil(
+       // pre-cache static assets
+       caches.open('static')
+           .then((cache) => {
+               cache.addAll([
+                   '/',
+                   '/index.html',
+                   '/src/js/app.js',
+                   '/src/css/app.css',
+                   '/src/images/otd.png',
+                   'https://fonts.googleapis.com/css?family=Noto+Serif'
+               ]);
+           })
+           .catch( err => {
+               console.log(err);
+           })
+   );
 });
 
 self.addEventListener('activate', () => {
     console.log('service worker activated');
+});
+
+self.addEventListener('fetch', (event) => {
+   event.respondWith(
+       caches.match(event.request)
+           .then((res) => {
+               if(res) {
+                   return res;
+               } else {
+                   return fetch(event.request);
+               }
+           })
+   );
 });
