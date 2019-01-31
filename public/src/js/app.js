fc16ae1900489084ed7c46becc4008d4d47c9e7e
Daniel Barclay <daniel@urbanintel.io>
2019-01-31 14:51:33 +0000

    HTML, CSS and JS for dynamically displaying content of PWA

---
+++ public/src/js/app.js
@@ -0,0 +1,26 @@
+const pwaCard = document.querySelector('#pwa');
+const pwaCardContent = pwaCard.querySelector('.card__content');
+const pwaCardDetails = pwaCard.querySelector('.card__details');
+let detailsShown = false;
+
+pwaCard.addEventListener('click', (() => {
+  if (!detailsShown) {
+    detailsShown = true;
+    pwaCardContent.style.opacity = 0;
+    pwaCardDetails.style.display = 'block';
+    pwaCardContent.style.display = 'none';
+    setTimeout(function () {
+      pwaCardDetails.style.opacity = 1;
+    }, 300);
+  } else {
+    detailsShown = false;
+    pwaCardDetails.style.opacity = 0;
+    pwaCardContent.style.display = 'block';
+    pwaCardDetails.style.display = 'none';
+    setTimeout(function () {
+      pwaCardContent.style.opacity = 1;
+    }, 300);
+  }
+}));
+
+document.getElementById("date").innerHTML = new Date().toDateString().slice(0, -4);
